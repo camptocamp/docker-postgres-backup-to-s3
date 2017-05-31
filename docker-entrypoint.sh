b82985d0040621b24c60c936d2f104916cfd807b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-05-31 13:54:18 +0200

    Fix metrics

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,10 @@
 
 pg_dump -Fc | aws s3 cp - "s3://${AWS_S3_BUCKET}/postgres.${PGDATABASE}.$(date +%Y%m%d-%H%M).dump"
 
-pg_dump_status=${PIPESTATUS[0]}
-aws_status=${PIPESTATUS[1]}
+statuses=("${PIPESTATUS[@]}")
+pg_dump_status=${statuses[0]}
 echo "pg_dump exited with status: ${pg_dump_status}"
+aws_status=${statuses[1]}
 echo "aws exited with status: ${aws_status}"
 
 stats=$(aws s3 ls --summarize --recursive "${AWS_S3_BUCKET}")
@@ -16,14 +17,11 @@ echo "Total Size of bucket: ${size}"
 
 if [ ! -z "${PUSHGATEWAY_URL}" ]; then
   echo "Sending metrics to ${PUSHGATEWAY_URL}"
-  cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/postgres_s3_backup/${PG_DATABASE}"
-# TYPE pg_dump_status counter
-pg_dump_status ${pg_dump_status}
-# TYPE aws_status counter
-aws_status ${aws_status}
-# TYPE objects counter
-objects ${objects}
-# TYPE size counter
-size ${size}
+  cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/postgres_s3_backup/database/${PGDATABASE}"
+# TYPE postgres_s3_backup counter
+postgres_s3_backup{what="pg_dump_status"} ${pg_dump_status}
+postgres_s3_backup{what="aws_status"} ${aws_status}
+postgres_s3_backup{what="total_objects"} ${objects}
+postgres_s3_backup{what="total_size"} ${size}
 EOF
 fi
