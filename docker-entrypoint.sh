9d58ddba8d2bdb94a1b7dd0f8735993b6430cfb3
Léo Depriester <leo.depriester@camptocamp.com>
2018-05-07 11:08:38 +0200

    Backup all databases, add blacklisting feature

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-pg_dump -Fc | aws s3 cp - "s3://${AWS_S3_BUCKET}/postgres.${PGDATABASE}.$(date +%Y%m%d-%H%M).dump"
+list_databases() {
+  blacklisted_databases=$(echo "$BLACKLISTED_DATABASES" | sed -e "s/,/'&'/g" -e "s/^/\'/" -e "s/$/'/")
+  databases=$(psql -t -A -c "SELECT datname FROM pg_database WHERE datname not in ($blacklisted_databases)")
+}
 
-statuses=("${PIPESTATUS[@]}")
-pg_dump_status=${statuses[0]}
-echo "pg_dump exited with status: ${pg_dump_status}"
-aws_status=${statuses[1]}
-echo "aws exited with status: ${aws_status}"
+backup_database() {
+  pg_dump -Fc | aws s3 cp - "s3://${AWS_S3_BUCKET}/postgres.${PGDATABASE}.$(date +%Y%m%d-%H%M).dump"
 
-stats=$(aws s3 ls --summarize --recursive "${AWS_S3_BUCKET}")
-objects=$(sed -n '/ *Total Objects: / s///p' <<<"${stats}")
-echo "Total Objects in bucket: ${objects}"
+  statuses=("${PIPESTATUS[@]}")
+  pg_dump_status=${statuses[0]}
+  echo "pg_dump exited with status: ${pg_dump_status}"
+  aws_status=${statuses[1]}
+  echo "aws exited with status: ${aws_status}"
 
-size=$(sed -n '/ *Total Size: / s///p' <<<"${stats}")
-echo "Total Size of bucket: ${size}"
+  stats=$(aws s3 ls --summarize --recursive "${AWS_S3_BUCKET}")
+  objects=$(sed -n '/ *Total Objects: / s///p' <<<"${stats}")
+  echo "Total Objects in bucket: ${objects}"
 
-if [ ! -z "${PUSHGATEWAY_URL}" ]; then
-  echo "Sending metrics to ${PUSHGATEWAY_URL}"
+  size=$(sed -n '/ *Total Size: / s///p' <<<"${stats}")
+  echo "Total Size of bucket: ${size}"
 
-  if getent hosts rancher-metadata >/dev/null; then
-    instance=$(curl http://rancher-metadata/latest/self/container/name)
-  else
-    instance=$(hostname -f)
-  fi
+  if [ ! -z "${PUSHGATEWAY_URL}" ]; then
+    echo "Sending metrics to ${PUSHGATEWAY_URL}"
 
-  cat <<EOF | curl -s --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/postgres_s3_backup/instance/${instance}/database/${PGDATABASE}"
+    if getent hosts rancher-metadata >/dev/null; then
+      instance=$(curl http://rancher-metadata/latest/self/container/name)
+    else
+      instance=$(hostname -f)
+    fi
+
+    cat <<EOF | curl -s --data-binary @- "${PUSHGATEWAY_URL}/metrics/job/postgres_s3_backup/instance/${instance}/database/${PGDATABASE}"
 # TYPE postgres_s3_backup_pg_dump_status gauge
 postgres_s3_backup_pg_dump_status ${pg_dump_status}
 # TYPE postgres_s3_backup_aws_status gauge
@@ -37,3 +43,11 @@ postgres_s3_backup_total_size ${size}
 postgres_s3_backup_end_time $(date +%s)
 EOF
 fi
+}
+
+list_databases
+
+for PGDATABASE in ${databases[@]}; do
+  echo "[*] Backuping ${PGDATABASE}"
+  backup_database
+done
