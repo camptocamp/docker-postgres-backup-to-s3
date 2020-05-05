8908ecfe338991b48f98a77cf2855202c0a54621
Julien Acroute <julien.acroute@camptocamp.com>
2020-05-05 11:51:05 +0200

    Multipart upload

--- pipe.py
+++ pipe.py
@@ -1,22 +1,60 @@
 from subprocess import Popen, PIPE
+import os
 
-input_cmd = "cat /home/jacroute/Bureau/IMG_20200326_091927876.jpg"
-output_cmd = "tee /dev/null"
+import boto3
 
-print("%s --> %s" % (input_cmd, output_cmd))
+BUFFER_SIZE = 10 * 1024 * 1024
+BUFFER = bytearray(BUFFER_SIZE)
+
+#os.environ["HTTP_PROXY"] = "http://localhost:3128"
+
+AWS_ENDPOINT = 'http://localhost:9000'
+AWS_ACCESS_KEY = 'AKIAACCESSKEY'
+AWS_SECRET_KEY = 'SECRETSECRET'
+AWS_BUCKET = 'postgres-to-s3'
+AWS_OBJECT_KEY = 'IMG_20200326_091927876.jpg'
+AWS_MULTIPART_UPLOAD_ID = 'postgres-to-s3-IMG_20200326_091927876.jpg'
+
+AWS_TARGET = {'Bucket': AWS_BUCKET, 'Key': AWS_OBJECT_KEY}
+client = boto3.client('s3',
+                      endpoint_url=AWS_ENDPOINT,
+                      aws_access_key_id=AWS_ACCESS_KEY,
+                      aws_secret_access_key=AWS_SECRET_KEY)
+
+multipart_upload = client.create_multipart_upload(**AWS_TARGET)
+
+input_cmd = "cat /home/jacroute/git/github.com/camptocamp/docker-postgres-backup-to-s3/data"
+
+print("%s --> S3" % input_cmd)
 bytes_written = 0
-with Popen(output_cmd.split(), stdin=PIPE, stderr=PIPE) as output:
-    with Popen(input_cmd.split(), stdout=PIPE, stderr=PIPE) as input:
-        while True:
-            buffer = input.stdout.read1()
-            if len(buffer) == 0:
-                break
-            bytes_written += len(buffer)
-            print('Write %s bytes' % len(buffer))
-            output.stdin.write(buffer)
-        if input.poll() is not None:
-            output.stdin.close()
-            output.wait()
-            print("%s bytes written" % bytes_written)
+with Popen(input_cmd.split(), stdout=PIPE, stderr=PIPE) as input:
+    part_number = 1
+    parts = []
+    while True:
+
+        # Retrieve data from input in the buffer
+        bytes_read = input.stdout.readinto(BUFFER)
+        if bytes_read == 0:
+            break
+
+        # Push buffer to object storage
+        res = client.upload_part(**AWS_TARGET,
+                                 UploadId=multipart_upload['UploadId'],
+                                 PartNumber=part_number,
+                                 Body=BUFFER if bytes_read == BUFFER_SIZE else BUFFER[0:bytes_read],
+                                 ContentLength=bytes_read)
+
+        print(res)
+        parts.append({'ETag': res['ETag'], 'PartNumber': part_number})
+        bytes_written += bytes_read
+        print('Write %s bytes' % bytes_read)
+
+        part_number += 1
+    if input.poll() is not None:
+        res = client.complete_multipart_upload(**AWS_TARGET,
+                                               MultipartUpload={'Parts': parts},
+                                               UploadId=multipart_upload['UploadId'])
+        print(res)
+        print("%s bytes written" % bytes_written)
 
 print("Done")
