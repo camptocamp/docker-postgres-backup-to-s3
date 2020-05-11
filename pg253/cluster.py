a6491ca8af66c6d708c91e280ab40e66f5e191e3
Julien Acroute <julien.acroute@camptocamp.com>
2020-05-11 16:59:49 +0200

    Implement db exclude parameter

--- pg253/cluster.py
+++ pg253/cluster.py
@@ -1,11 +1,23 @@
+import re
+import os
+from subprocess import run
+
 from pg253.transfer import Transfer
+from pg253.configuration import Configuration
 
 class Cluster:
     def __init__(self, metrics):
         self.metrics = metrics
+        self.db_exclude = re.compile(os.environ[Configuration.BLACKLISTED_DATABASES])
+        print("OK")
 
     def listDatabase(self):
-        return ['postgres']
+        cmd = ['psql', '-qAtX', '-c', '"SELECT datname FROM pg_database"']
+        res = run(cmd, capture_output=True)
+        dbs = res.stdout.decode().strip().split("\n")
+        dbs = list(filter(lambda x: not self.db_exclude.search(x), dbs))
+        dbs.remove('template0')
+        return dbs
 
     def backup(self):
         for database in self.listDatabase():
