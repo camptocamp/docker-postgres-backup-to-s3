a061a9bd1f986f90760b9303bd5d7f729a90fd51
Julien Acroute <julien.acroute@camptocamp.com>
2020-05-11 16:58:45 +0200

    Add some dbs to test data

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,7 +1,32 @@
+-- Postgres DB
 CREATE TABLE data(
   id   serial,
   data text
 );
 
--- Generate a 50Go DB
-INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FROM generate_series(1, (6000000 / 438) * 50000) AS g (id);
+-- Generate a 5Go DB
+INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FROM generate_series(1, (6000000 / 438) * 5000) AS g (id);
+
+-- Application DB
+CREATE DATABASE application;
+\c application;
+
+CREATE TABLE data(
+  id   serial,
+  data text
+);
+
+-- Generate a 5Mo DB
+INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FROM generate_series(1, (6000000 / 438) * 5) AS g (id);
+
+-- Data DB
+CREATE DATABASE data;
+\c data;
+
+CREATE TABLE data(
+  id   serial,
+  data text
+);
+
+-- Generate a 500Mo DB
+INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FROM generate_series(1, (6000000 / 438) * 500) AS g (id);
